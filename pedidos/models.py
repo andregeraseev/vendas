ed69e83bb6e4c6a2b7a063b233bd230562c945e4
Andre Geraseev <ageraseev@gmail.com>
2022-11-12 00:11:46 -0300

    valores unitarios

--- pedidos/models.py
+++ pedidos/models.py
@@ -12,7 +12,44 @@ class Item(models.Model):
 
     @property
     def valor_total(self):
+        qr_item = [it.items.all() for it in self.pedido_set.all()]
+        total = []
+        for items in qr_item:
+            for item  in items:
+                total.append(item.quantidade)
+
+        quantidade= sum(total)
+        if quantidade >= 20 and quantidade <= 49:
+            return 22 * self.quantidade
+        elif quantidade >= 50 and quantidade <= 99:
+            return 20 * self.quantidade
+        elif quantidade >= 100 and quantidade <= 199:
+            return 18 * self.quantidade
+        elif quantidade >= 200:
+            return 17 * self.quantidade
+        else:
             return self.quantidade * 25
+
+    def valor_unitario(self):
+        qr_item = [it.items.all() for it in self.pedido_set.all()]
+        total = []
+        for items in qr_item:
+            for item in items:
+                total.append(item.quantidade)
+
+        quantidade = sum(total)
+        if quantidade >= 20 and quantidade <= 49:
+            return 22
+        elif quantidade >= 50 and quantidade <= 99:
+            return 20
+        elif quantidade >= 100 and quantidade <= 199:
+            return 18
+        elif quantidade >= 200:
+            return 17
+        else:
+            return 25
+
+
 
 
 
@@ -41,7 +78,7 @@ class Pedido(models.Model):
         if quantidade >=20 and quantidade  <=49:
             return 22 * quantidade
         elif quantidade >=50 and quantidade  <=99:
-            return 10 * quantidade
+            return 20 * quantidade
         elif quantidade >=100 and quantidade  <=199:
             return 18 * quantidade
         elif quantidade >= 200:
@@ -49,6 +86,18 @@ class Pedido(models.Model):
         else:
             return quantidade * 25
         # return sum([item.valor_total for item in self.items.all()])
+
+    @property
+    def unidades_total(self):
+        quantidade = sum([int(item.quantidade) for item in self.items.all()])
+
+        return quantidade
+
+    @property
+    def peso(self):
+        quantidade =  sum([int(item.quantidade) for item in self.items.all()])
+
+        return  round(quantidade * 0.04,2)
 
     @property
     def comicao(self):
